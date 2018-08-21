9f4b5cdee08b4117befe1bda51f82ab954df591d
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-21 13:56:42 +0200

    Correction avec la classe

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
   </head>
   <body>
     <!--Création d'un formulaire simple avec get en méthode-->
-<form class="formDeQualité" action="user.php" method="get">
-  <label for="lastName">Votre nom: </label><input type="text" name="lastName" placeholder="nom" />
-  <label for="firstName">Votre prénom: </label><input type="text" name="firstName" placeholder="prénom" />
-  <input type="submit" name="submit" />
-</form>
+    <form action="user.php" method="GET">
+      <label for="lastName">Votre nom: </label><input type="text" id="lastName" name="lastName" placeholder="nom" /><!--Champs nom-->
+      <label for="firstName">Votre prénom: </label><input type="text" id="firstName" name="firstName" placeholder="prénom" /><!--Champs prénom-->
+      <input type="submit" value="Page suivante" />
+      <!--Le clic "envoyer" enverra à user.php les données saisie dans l'url
+      car utilisation de GET dans la méthode du formulaire-->
+    </form>
   </body>
 </html>
